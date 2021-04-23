eaf4a029e8d020e4c926253d064e18d3f85518f6
Carlos Henrique Gomes <carlos.hgs@outlook.com.br>
2021-04-23 13:29:11 -0300

    tela cadastro equipamento, atualizacao envia dados

--- config/router.js
+++ config/router.js
@@ -13,34 +13,47 @@ module.exports = app => {
         .post(app.api.user.save)
         .get(app.api.user.get)
 
-    app.route('/users/:id')//alterar 
+    app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(admin(app.api.user.save)) 
+        .put(admin(app.api.user.save)) //alterar 
         .get(app.api.user.getById)
+
+    app.route('/sensor')
+        .post(app.config.passport.authenticate())
+        .post(app.api.sensor.save)//salva o sensor
+        .get(app.api.sensor.get)
+    
+    app.route('/sensor/:id')
+        .post(app.config.passport.authenticate())
+        .get(app.api.sensor.getByIdUser) //passa id do usuario checar sensores cadastrados
+        .put(app.api.sensor.save)//altera os dados do sensor, id do sensor
+   
+    app.route('/requisicaoSensor/:mac')
+        .post(app.config.passport.authenticate())
+        .get(app.api.sensor.getByMac)//sensor checa se foi cadastrado
 
     app.route('/dados')
         .post(app.config.passport.authenticate())
-        .get(app.api.dadosSensor.get)
-        .post(app.api.dadosSensor.save);
-
+        .post(app.api.dadosSensor.save); 
+    
     app.route('/dados/:id')
-        .all(app.config.passport.authenticate())
-        //.put(app.api.dadosSensor.saveStatus) 
-        .get(app.api.dadosSensor.getByIdRegistersDate)
+        .post(app.config.passport.authenticate())
+        .get(app.api.dadosSensor.getById) //id do sensor
+        .put(app.api.dadosSensor.save); // rota que será usada pelo hardware
 
      app.route('/medicamentos')
         .post(app.config.passport.authenticate())
-        .get(app.api.controleMedicamento.get)
-        .post(app.api.controleMedicamento.save);
+        .get(app.api.controleMedicamento.get) //todos medicamentos da base de dados
+        .post(app.api.controleMedicamento.save);//salva o medicamento
     
-    app.route('/medicamentos/:id')//alterar 
+    app.route('/medicamentos/:id')//id do medicamento
         .all(app.config.passport.authenticate())
-        .put(app.api.controleMedicamento.save) 
-        .get(app.api.controleMedicamento.getById)
+        .put(app.api.controleMedicamento.save) // altera medicamentos
+        .get(app.api.controleMedicamento.getById) // dados medicamento
         
     app.route('/medicamentos/user/:id')
         .all(app.config.passport.authenticate())
-        .get(app.api.controleMedicamento.getByIdUser)
+        .get(app.api.controleMedicamento.getByIdUser) //select com todo medicameno usuario
 
     app.route('/esquecisenha')
         .post(app.api.user.forgetPassword)
